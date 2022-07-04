475413aacd58771f6b25bf629b9777070b3fa716
Mr SHANZ <105359936+Itsme-shan@users.noreply.github.com>
2022-07-04 16:47:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,12 +9,12 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'V12',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'D62TzBfB',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autobio = true 
+global.autobio = false 
 
 //language
 //Available in indonesia & english
@@ -23,23 +23,23 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "SHANZ-BOT" //namabot kalian
+global.ownername= "么° 🄼 SHANZ 🥀 | ❤️" //nama kalian
+global.myweb ="https://youtube.com/channel/UCX9GuKbYYWJ80O3Kymkn3Uw" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCX9GuKbYYWJ80O3Kymkn3Uw" //bebas asal jan hapus
+global.github = "https://github.com/Itsme-shan" //bebas
+global.email = "malinduhirushan12345@gmail.com" //bebas
+global.region = "Sri Lanka" //bebas
+global.timezone = 'Asia/colombo' //  timezone wib
+global.owner = ["94725292890","94725292890"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94725292890" // nomor wa kalian
+global.ownernomerr = "+94725292890" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© MR-SHANZ' //sticker wm ubah
+global.author = 'MR SHANZ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -50,17 +50,17 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+    sukses: '🤗Done,',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'This command can only be used when the bot is admin',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'This command can only be used in groups !',
+    private: 'This command can only be used in private !',
+	bot: 'Bot Number User Special Features',
+    errtoimg: 'Sorry Currently Not Support Gif Sticker !',
+    wait: '⏳ Being processed',
+	lockCmd: 'Features Not Activated By Owner!',
+	example1: 'Welcome @user By Group @subject Dont forget to read the rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
 let file = require.resolve(__filename)
